afe889dd63d92cf14100cdb0f006f924dbc5a50b
Guo Yunhe <i@guoyunhe.me>
2018-10-21 10:03:40 +0300

    style

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -9,9 +9,10 @@ class Image extends Component {
       <div className="Image">
         <img className="Image__image" src={url} alt="preview" />
         <div className="Image__info">
-          {modified && <span className="Image__alert">⚠ Photo might be modified too much</span>}
-          {downloaded && <span className="Image__alert">️⚠ Photo is from the internet</span>}
-          {converted && <span className="Image__alert">️⚠ Photo is converted before uploading</span>}
+          {modified && <div className="Image__alert">😨 Photo might be modified too much</div>}
+          {downloaded && <div className="Image__alert">️🤔 Photo is from the internet</div>}
+          {converted && <div className="Image__note">️😉 Photo is converted before uploading</div>}
+          {!converted && !modified && !downloaded && <div className="Image__verify">️👍 The photo is original</div>}
         </div>
         {this.props.delete && <button className="Image__delete-button" onClick={() => this.props.delete(this.props.image)}>Delete</button>}
       </div>
